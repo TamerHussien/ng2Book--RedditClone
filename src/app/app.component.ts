e83e6b8d88885f6d991f9b1c16108f44e8f69568
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-04-27 19:41:16 +0200

    Finish the 2nd Project :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
-
+import { Article } from './shared/article.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+articles: Article[];
 
+constructor () {
+  this.articles = [
+    new Article('Angular 2', 'http://angular.io', 3),
+    new Article('Fullstack', 'http://fullstack.io', 2),
+    new Article('Angular Homepage', 'http://angular.io', 1)
+  ];
+}
   addArticle(title: HTMLInputElement, link: HTMLInputElement) {
 
     console.log(`Adding Article: ${title.value}  and Link: ${link.value}`);
-
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortArticles() {
+
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import { Article } from '../shared/article.model';
 @Component({
   selector: 'app-article',
@@ -7,23 +7,21 @@ import { Article } from '../shared/article.model';
 })
 export class ArticleComponent implements OnInit {
 @HostBinding('attr.class') cssClass = 'row';
-article: Article;
+@Input() article: Article;
   constructor() {
 
-    this.article = new Article('Angular 4', 'http://angular.io', 10);
   }
 
   ngOnInit() {
   }
-voteUp() {
-  this.article.votes += 1;
+voteUp(): boolean {
+  this.article.voteUp();
   return false; // this to stop page reloading
 
 }
 
-voteDown() {
-
-  this.article.votes -= 1;
+voteDown(): boolean {
+  this.article.voteDown();
   return false; // this to stop page reloading
 }
 }
