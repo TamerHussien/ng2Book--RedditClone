2d3cf189e3add5af75cc2f8cac703fc340e45a25
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-04-27 16:33:20 +0200

    update app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement) {
+
+    console.log(`Adding Article: ${title.value}  and Link: ${link.value}`);
+
+    return false;
+  }
 }
